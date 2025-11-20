419412875551c887ed1f87e51ae40f90bbb70847
Akash mande <aakashmande21@gmail.com>
2025-11-20 13:17:12 +0530

    Initial Commit

---
+++ src/main/java/com/JSBP/E_Notes_service/ENotesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.JSBP.E_Notes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENotesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENotesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JSBP/E_Notes_service/ENotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JSBP.E_Notes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENotesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
